bd15e6cdefeabcd20801995026cda5b755f5267c
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-17 13:54:04 -0300

    Criacao da classe OrderItem

---
+++ Exer_Prop/Entities/Class/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exer_Prop.Entities.Class
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        // Criação do metodo SubTotal
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+
+}
