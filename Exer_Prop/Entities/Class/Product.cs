08817df5752ab6704bae55e0c56f5ccc0bd26b7a
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-17 13:45:29 -0300

    Criacao da classe Product

---
+++ Exer_Prop/Entities/Class/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exer_Prop.Entities.Class
+{
+    class Product
+    {
+        public string Name { get; set; } // Criação da proprieti autoimplementada do atributo Name da classe Product 
+        public double Price { get; set; }
+
+        //Criação do construtor padrão para a classe Product 
+        public Product()
+        {
+
+        }
+        // Criação do construtor customizado para a classe Product 
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
